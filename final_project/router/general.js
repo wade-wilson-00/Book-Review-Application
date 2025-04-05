f491b21f18058eb91fb65d570a8249c1d11646e2
Aman Gujamagadi <caffeineaddict00dev@gmail.com>
2025-04-06 01:00:04 +0530

    Update general.js

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -1,13 +1,32 @@
-const express = require('express');
+const express = require("express");
+const axios = require("axios");
 let books = require("./booksdb.js");
 let isValid = require("./auth_users.js").isValid;
 let users = require("./auth_users.js").users;
 const public_users = express.Router();
 
+const doesExist = (username) => {
+  return users.some((user) => user.username === username);
+};
 
-public_users.post("/register", (req,res) => {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+const getAllBooks = () => {
+  return books;
+};
+
+public_users.post("/register", (req, res) => {
+  const username = req.body.username;
+  const password = req.body.password;
+
+  if (!username || !password) {
+    return res.status(404).json({ message: "Missing username or password" });
+  } else if (doesExist(username)) {
+    return res.status(404).json({ message: "user already exists." });
+  } else {
+    users.push({ username: username, password: password });
+    return res
+      .status(200)
+      .json({ message: "User successfully registered.  Please login." });
+  }
 });
 
 // Get the book list available in the shop
@@ -30,7 +49,7 @@ public_users.get("/isbn/:isbn", async (req, res) => {
     return res.status(200).json(targetBook);
   }
 });
-  
+
 // Get book details based on author
 public_users.get("/author/:author", async (req, res) => {
   // get array of matching book objects
@@ -66,4 +85,5 @@ public_users.get("/review/:isbn", function (req, res) {
     return res.status(404).json({ message: "ISBN not found." });
   }
 });
+
 module.exports.general = public_users;
