4460b58a548d5cd72e32d5359e070f06571f3c4e
Aman Gujamagadi <caffeineaddict00dev@gmail.com>
2025-04-06 00:57:57 +0530

    Update index.js

--- final_project/index.js
+++ final_project/index.js
@@ -1,12 +1,21 @@
-const express = require('express');
-const jwt = require('jsonwebtoken');
-const session = require('express-session')
-const customer_routes = require('./router/auth_users.js').authenticated;
-const genl_routes = require('./router/general.js').general;
+const express = require("express");
+const jwt = require("jsonwebtoken");
+const session = require("express-session");
+const customer_routes = require("./router/auth_users.js").authenticated;
+const genl_routes = require("./router/general.js").general;
 
 const app = express();
 
 app.use(express.json());
+
+app.use(
+  "/customer",
+  session({
+    secret: "fingerprint_customer",
+    resave: true,
+    saveUninitialized: true,
+  })
+);
 
 app.use("/customer/auth/*", function auth(req, res, next) {
   if (req.session.authorization) {
@@ -24,10 +33,10 @@ app.use("/customer/auth/*", function auth(req, res, next) {
     return res.status(403).json({ message: "User not logged in." });
   }
 });
- 
-const PORT =5000;
+
+const PORT = process.env.PORT || 1337;
 
 app.use("/customer", customer_routes);
 app.use("/", genl_routes);
 
-app.listen(PORT,()=>console.log("Server is running"));
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
